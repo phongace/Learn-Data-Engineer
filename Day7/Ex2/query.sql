f98a393d07eb94ad9d20753a50773e670d68409f
Hoang Phong <phongnh2@smartosc.com>
2025-05-06 08:55:53 +0700

    Day 7

---
+++ Day7/Ex2/query.sql
@@ -0,0 +1,26 @@
+CREATE OR REPLACE TABLE `kestra-phongnh2.sales_dataset.sales_partitioned`
+PARTITION BY target_close_date AS
+SELECT
+  Date AS created_date,
+  Salesperson,
+  `Lead Name`,
+  Segment,
+  Region,
+  `Target Close` AS target_close_date,
+  `Forecasted Monthly Revenue`,
+  `Opportunity Stage`,
+  `Weighted Revenue`,
+  `Closed Opportunity`,
+  `Active Opportunity`,
+  `Latest Status Entry`
+FROM `kestra-phongnh2.sales_dataset.sales`;
+
+-- Filter by Partition Range 
+SELECT
+  Region,
+  COUNT(*) AS total_opportunities,
+  SUM(`Weighted Revenue`) AS total_weighted_revenue
+FROM `kestra-phongnh2.sales_dataset.sales_partitioned`
+WHERE target_close_date BETWEEN '2011-01-01' AND '2011-06-30'
+GROUP BY Region
+ORDER BY total_weighted_revenue DESC;
